ed37f22dce989ae797156de08a12f26baca4bb79
Dalton Dick <daltondick2010@gmail.com>
2016-03-28 21:02:51 -0500

    Generate room model

---
+++ blue_sky/priv/repo/migrations/20160329020106_create_room.exs
@@ -0,0 +1,12 @@
+defmodule BlueSky.Repo.Migrations.CreateRoom do
+  use Ecto.Migration
+
+  def change do
+    create table(:rooms, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      timestamps
+    end
+
+  end
+end
