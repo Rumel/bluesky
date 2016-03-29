63c0643c7724bc93f8fbfcc6d47b203f5f42f743
Dalton Dick <daltondick2010@gmail.com>
2016-03-28 21:02:41 -0500

    Remove username and password for database

--- blue_sky/config/dev.exs
+++ blue_sky/config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :blue_sky, BlueSky.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "blue_sky_dev",
   hostname: "localhost",
   pool_size: 10
