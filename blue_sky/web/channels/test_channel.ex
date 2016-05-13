9d72fda1e70a5ea653e1aaf7ae5a87e964351660
bigcakes <jjjsgmw@hotmail.com>
2016-05-12 22:18:56 -0400

    Got a simple chat application working on the home page using channels.

--- blue_sky/web/channels/test_channel.ex
+++ blue_sky/web/channels/test_channel.ex
@@ -4,6 +4,22 @@ defmodule BlueSky.TestChannel do
   # Documentation
   # http://www.phoenixframework.org/docs/channels
   
+  def join("test:lobby", _params, socket) do
+     {:ok, socket}
+  end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body}
+    {:noreply, socket}
+  end
+
+  def handle_out("new_msg", payload, socket) do
+    push socket, "new_msg", payload
+    {:noreply, socket}
+  end
+
+  
+
   def join("test:test", _params, socket) do
      {:ok, %{ data: "Connected", socket: socket}}
   end
