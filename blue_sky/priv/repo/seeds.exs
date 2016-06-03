de76ed39559cae703189f46a6cd2c9d79dd52526
bigcakes <jjjsgmw@hotmail.com>
2016-06-03 14:54:54 -0400

    Changed styling/format and added new seed questions.

--- blue_sky/priv/repo/seeds.exs
+++ blue_sky/priv/repo/seeds.exs
@@ -41,3 +41,84 @@ Repo.insert!(%Question{
   d: "St. Louis Cardinals",
   answer: "d"
 })
+
+Repo.insert!(%Question{
+  question: "What is the Spanish word for 'dog'?",
+  a: "gato",
+  b: "rojo",
+  c: "perro",
+  d: "casa",
+  answer: "c"
+})
+Repo.insert!(%Question{
+  question: "Who wrote the popular book series 'The Hunger Games'?",
+  a: "Suzanne Collins",
+  b: "Stephanie Meyer",
+  c: "J. K. Rowling",
+  d: "K. A. Applegate",
+  answer: "a"
+})
+Repo.insert!(%Question{
+  question: "Which Constitutional Amendment granted women the right to vote?",
+  a: "11th",
+  b: "19th",
+  c: "20th",
+  d: "21st",
+  answer: "b"
+})
+Repo.insert!(%Question{
+  question: "Which actor played the character Gandalf in the 'Lord of the Rings' movie trilogy?",
+  a: "Ian McKellan",
+  b: "Elijah Wood",
+  c: "Sean Connery",
+  d: "Patrick Stewart",
+  answer: "a"
+})
+Repo.insert!(%Question{
+  question: "Which actor did NOT appear in the HBO TV series 'The Sopranos'?",
+  a: "Edie Falco",
+  b: "James Gandolfini",
+  c: "Steven Van Zandt",
+  d: "Al Pacino",
+  answer: "d"
+})
+Repo.insert!(%Question{
+  question: "What is the birthstone for the month of September?",
+  a: "emerald",
+  b: "ruby",
+  c: "peridot",
+  d: "sapphire",
+  answer: "d"
+})
+Repo.insert!(%Question{
+  question: "Which of the following is the longest river in Europe?",
+  a: "Volga",
+  b: "Danube",
+  c: "Nile",
+  d: "Rhine",
+  answer: "a"
+})
+Repo.insert!(%Question{
+  question: "Which color does NOT appear in the German flag?",
+  a: "red",
+  b: "white",
+  c: "gold",
+  d: "black",
+  answer: "b"
+})
+Repo.insert!(%Question{
+  question: "Which of the following is not one of Michigan's Great Lakes?",
+  a: "Lake Huron",
+  b: "Lake Michigan",
+  c: "Lake St. Clair",
+  d: "Lake Erie",
+  answer: "c"
+})
+Repo.insert!(%Question{
+  question: "Of the following, which animal has the longest life span?",
+  a: "human",
+  b: "elephant",
+  c: "blue whale",
+  d: "giant tortoise",
+  answer: "d"
+})
