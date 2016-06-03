f4746c4a32ed2154a39cd11682d7105908ff070d
Bob Sakson <bsakson@paylocity.com>
2016-06-03 08:01:28 -0500

    Changed game over event to hide remainder of the screen content for now

--- client/app/components/app.component.ts
+++ client/app/components/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { SignUpComponent } from './signup.component';
 import { QuestionComponent } from './question.component';
 import { RoomComponent } from './room.component';
-import { ResultComponent } from './result.component';
 import { TriviaService } from '../services/trivia.service';
 import { SignUpService } from '../services/signup.service';
 import { PlayerService } from '../services/player.service';
@@ -33,11 +32,6 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
     path: '/room/',
     name: 'Room',
     component: RoomComponent
-  },
-  {
-    path: '/result/:question/:answer',
-    name: 'Result',
-    component: ResultComponent
   }
 ])
 
