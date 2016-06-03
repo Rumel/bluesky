846dd8eac24ec669d7d5c9327f97edef7a1d1439
bigcakes <jjjsgmw@hotmail.com>
2016-06-03 15:29:52 -0400

    Added missing dev dependencies and changed gulpfile.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,14 +1,43 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
+var jspm = require('gulp-jspm-build');
 
 gulp.task('prep', function() {
-    exec('npm install | jspm init');
+    exec('npm install');
 });
 
-gulp.task('build_dev', function() {
-    exec('jspm bundle-sfx ./app/main.js app.js | npm run lite');
+
+gulp.task('build_dev', function(){
+    jspm({
+      bundleOptions: {
+        minify: false,
+        mangle: false
+      },
+      bundles: [
+        { src: './app/main.js', dst: 'app.js' }
+      ],
+      config: "",
+      bundleSfx: true
+    })
+    .pipe(gulp.dest('./'));
+
+    exec('npm run lite');
 });
 
-gulp.task('build_prod', function() {
-    exec('jspm bundle-sfx ./app/main.js app.js --minify | npm run lite');
+gulp.task('build_prod', function(){
+    jspm({
+      bundleOptions: {
+        minify: true,
+        mangle: true
+      },
+      bundles: [
+        { src: './app/main.js', dst: 'app.js' }
+      ],
+      config: "",
+      bundleSfx: true
+    })
+    .pipe(gulp.dest('./'));
+
+    exec('npm run lite');
 });
+
